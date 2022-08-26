fb7d9924893e03139428be52be5f48820d070454
Topy <gmsdl1994@naver.com>
2022-08-27 03:37:31 +0900

    Create project

---
+++ src/main/java/Study/JpaStudy_03/JpaStudy03Application.java
@@ -0,0 +1,13 @@
+package Study.JpaStudy_03;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaStudy03Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaStudy03Application.class, args);
+	}
+
+}
---
+++ src/test/java/Study/JpaStudy_03/JpaStudy03ApplicationTests.java
@@ -0,0 +1,13 @@
+package Study.JpaStudy_03;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaStudy03ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
