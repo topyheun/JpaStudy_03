a1d670e87a1ea25a165732505ca30d8d9a692373
Topy <gmsdl1994@naver.com>
2022-08-29 23:57:23 +0900

    Session 08 ~ 11

--- src/main/java/Study/JpaStudy_03/repository/MemberRepository.java
+++ src/main/java/Study/JpaStudy_03/repository/MemberRepository.java
@@ -3,6 +3,9 @@ package Study.JpaStudy_03.repository;
 import Study.JpaStudy_03.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
+    List<Member> findByUsernameAndAgeGreaterThan(String username, int age);
 }
